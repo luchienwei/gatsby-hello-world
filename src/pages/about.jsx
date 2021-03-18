3477cb0a4d0e4e3bf0a8efde105a3c4907f73e85
luchienwei <luchienwei@gmail.com>
2021-03-17 21:03:01 -0400

    first commit

---
+++ src/pages/about.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <div>
+            <h1>About This Page</h1>
+            <p> wtf </p>
+        </div>
+    )
+}
